fc1eb9f6132fbf7e16c9d3386b6fed391fd43626
deeparka <arkadeep180@gmail.com>
2022-10-15 22:26:58 +0530

    build app

--- sanity.js
+++ sanity.js
@@ -2,7 +2,7 @@ import { createCurrentUserHook, createClient } from "next-sanity";
 import createImageUrlBuilder from "./sanitymedium/node_modules/@sanity/image-url";
 
 export const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASE || "production",
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "2021-03-25",
     useCdn: process.env.NODE_ENV === "production",
